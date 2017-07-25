34552feec8663e360706f5431aac631f6fe652b1
Com06 <com06@Com06s-iMac.local>
2017-07-25 16:03:56 +0700

    Initial Commit

---
+++ MoneyTeam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoneyTeam
+//
+//  Created by Com06 on 7/25/2560 BE.
+//  Copyright © 2560 team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
